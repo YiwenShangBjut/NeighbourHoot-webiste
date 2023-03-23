63cdf03f5348a8c137afb5427d27e77a281a1b75
root <root@DESKTOP-06GVMKA.localdomain>
2023-03-23 23:07:22 +0000

    update router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ClientPage from "./pages/ClientPage";
 import AdminPage from "./pages/AdminPage";
 import SplashPage from "./pages/SplashPage";
@@ -7,18 +7,14 @@ import UserTable from "./pages/UserTable";
 import "./styles.css";
 
 function App() {
-  
+
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<SplashPage />} />
-          <Route path="/user" exact element={<UserTable />} />
-          <Route path="/create" exact element={<ClientPage />} />
-          <Route path="/admin" element={ <AdminPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <Routes>
+      <Route path="/" exact element={<SplashPage />} />
+      <Route path="/user" element={<UserTable />} />
+      <Route path="/create" element={<ClientPage />} />
+      <Route path="/admin" element={<AdminPage />} />
+    </Routes>
   );
 }
 export default App;
