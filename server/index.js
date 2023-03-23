a6f82f09c07dddbcc9c5b239fd5e3f9565c8d20f
root <root@DESKTOP-06GVMKA.localdomain>
2023-03-23 15:11:37 +0000

    update server

--- server/index.js
+++ server/index.js
@@ -20,11 +20,12 @@ app.post("/create", (req, res) => {
   const condition_cat = req.body.condition_cat;
   const price = req.body.price;
   const description = req.body.description;
-  console.log(name);
+  const user_id=req.body.user_id
+  const status=req.body.status
 
   db.query(
-    "INSERT INTO barter (name, category, condition_cat, price, description) VALUES (?,?,?,?,?)",
-    [name, category, condition_cat, price, description],
+    "INSERT INTO barter (name, category, condition_cat, price, description, user_id, status) VALUES (?,?,?,?,?,?,?)",
+    [name, category, condition_cat, price, description, user_id, status],
     (err, result) => {
       if (err) {
         console.log(err);
@@ -56,21 +57,49 @@ app.get("/barter/:id", (req, res)=>{
       });
 });
 
-// app.put("/update", (req, res) => {
-//   const id = req.body.id;
-//   const description = req.body.description;
-//   db.query(
-//     "UPDATE barter SET description = ? WHERE id = ?",
-//     [description, id],
-//     (err, result) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         res.send(result);
-//       }
-//     }
-//   );
-// });
+app.get("/user/:name", (req, res)=>{
+  const name = req.params.name;
+  db.query("SELECT * FROM user WHERE user.name=?",name, (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+});
+
+app.put("/update/barter", (req, res) => {
+  const id = req.body.id;
+  const status = req.body.status;
+  db.query(
+    "UPDATE barter SET status = ? WHERE id = ?",
+    [status, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.put("/update/user", (req, res) => {
+  const id = req.body.id;
+  const points = req.body.points;
+  db.query(
+    "UPDATE user SET points = ? WHERE id = ?",
+    [points, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
 
 // app.delete("/delete/:id", (req, res) => {
 //   const id = req.params.id;
